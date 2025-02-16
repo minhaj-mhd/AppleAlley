68e1ec1a4788a2e9858e53addd3e53332c13b27a
minhaj-mhd <minhajmuhamd@gmail.com>
2025-02-16 22:14:30 +0530

    adding sec

--- ecom/settings.py
+++ ecom/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+
+# Initialize environment variables
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^6@x+sjfxr^_^a=85=6!2r5=)5+9a&+)y!qhzw4i7=738hpkt-'
+SECRET_KEY = os.getenv("SECRET_KEY", default="1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["13.60.247.195",  "localhost"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -152,5 +158,5 @@ CACHES = {
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # settings.py
-RAZORPAY_KEY_ID = 'rzp_test_n2pbqGguu5XhZe'
-RAZORPAY_KEY_SECRET = 'KprPrrZf0Xj4QRY3efIyjLC6'
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
