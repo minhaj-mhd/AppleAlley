204399cbedea0c702fa6205cd0c7aa4e136b2bfe
minhaj-mhd <minhajmuhamad@gmail.com>
2025-02-27 15:35:03 +0530

    edited home layout

--- products/models.py
+++ products/models.py
@@ -60,6 +60,7 @@ class IPhoneVariant(models.Model):
     storage = models.ForeignKey(StorageOption, on_delete=models.CASCADE)
     color = models.ForeignKey(ColorOption, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    max_price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     stock_quantity = models.PositiveIntegerField(default=0)
     refurbished = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
