656897289f01d672b2ecc32db433c7d749548002
minhaj-mhd <minhajmuhamad@gmail.com>
2025-03-07 11:34:05 +0530

    add static root

--- ecom/settings.py
+++ ecom/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/ "static"
 ]
+STATIC_ROOT = '/var/www/gulf2/staticfiles/'
+
 MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join( BASE_DIR, "media/")
 
